a71d071ed2f08109bd671070856d4ba55154ce9b
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-05-08 16:37:12 -0600

    var

---
+++ lesson2/variables.js
@@ -0,0 +1,24 @@
+//variables
+
+//bounderies
+var bounderiesObj = {
+    top:0,
+    left:0,
+    bottom:548,
+    right:998
+};
+
+//changing DIV
+div1 = document.getElementById('div1');
+div2 = document.getElementById('div2');
+
+//select
+divSelect = document.getElementById('select-div');
+
+//buttons
+downBtn = document.getElementById('downBtn');
+upBtn = document.getElementById('upBtn');
+leftBtn = document.getElementById('leftBtn');
+rightBtn = document.getElementById('rightBtn');
+radioSizeBtn = document.getElementsByName('divSize');
+
