25f7a93264f6f8aa71e5acd5a9bb2f9db3c60281
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-05-09 14:07:15 -0600

    update var javascript

---
+++ lesson1/variables.js
@@ -0,0 +1,26 @@
+//variables
+
+var elemArray = Array();
+
+//bounderies
+var bounderiesArray = {
+    top:0,
+    left:0,
+    bottom:548,
+    right:998
+};
+
+//changing DIV
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+var div3 = document.getElementById('div3');
+var div4 = document.getElementById('div4');
+
+//select
+var divSelect = document.getElementById('select-div');
+
+//buttons
+var downBtn = document.getElementById('downBtn');
+var upBtn = document.getElementById('upBtn');
+var leftBtn = document.getElementById('leftBtn');
+var rightBtn = document.getElementById('rightBtn');
