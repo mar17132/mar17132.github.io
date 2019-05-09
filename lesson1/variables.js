1bc9aab0e7e47c5aa8ca90c93480667dd9a5f80f
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-05-09 15:04:29 -0600

    update var

--- lesson1/variables.js
+++ lesson1/variables.js
@@ -19,8 +19,13 @@ var div4 = document.getElementById('div4');
 //select
 var divSelect = document.getElementById('select-div');
 
+//input
+var borderColorTxt = document.getElementById('bcolorTxt');
+var backColorTxt = document.getElementById('backcolorTxt');
+
 //buttons
 var downBtn = document.getElementById('downBtn');
 var upBtn = document.getElementById('upBtn');
 var leftBtn = document.getElementById('leftBtn');
 var rightBtn = document.getElementById('rightBtn');
+var changeColorBtn = document.getElementById('colorChangeBtn');
