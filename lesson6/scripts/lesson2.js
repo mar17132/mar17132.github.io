eb0d088339e0775aaa0d6928b671a59e1fe0f991
mar17132 <mar17132@byui.edu>
2019-05-19 14:32:02 -0600

    6

---
+++ lesson6/scripts/lesson2.js
@@ -0,0 +1,140 @@
+
+
+
+
+function getSelectedDiv()
+{
+    if(divSelect.value != 'null')
+    {
+        return divSelect.value;
+    }
+
+    return null;
+}
+
+function getCSSPropVal(cssSelector,cssProp)
+{
+    var cssArrayObj = document.styleSheets;
+    var returnValue = false;
+
+    for(i = 0; i < cssArrayObj.length; i++)
+    {
+        var rulesArray = cssArrayObj[i].cssRules;
+
+        for(j = 0; j < rulesArray.length; j++)
+        {
+            var noSpaceSelector = rulesArray[j].selectorText.replace(' ','');
+
+            if(noSpaceSelector == cssSelector)
+            {
+                returnValue = rulesArray[j].style[cssProp];
+            }
+        }
+    }
+
+    return returnValue;
+}
+
+function setCSSPropVal(cssSelector,cssProp,cssVal)
+{
+    var cssArrayObj = document.styleSheets;
+    var returnValue = false;
+
+    for(i = 0; i < cssArrayObj.length; i++)
+    {
+        var rulesArray = cssArrayObj[i].cssRules;
+
+        for(j = 0; j < rulesArray.length; j++)
+        {
+            var noSpaceSelector = rulesArray[j].selectorText.replace(' ','');
+
+            if(noSpaceSelector == cssSelector)
+            {
+                rulesArray[j].style.setProperty(cssProp,cssVal);
+            }
+        }
+    }
+
+    return returnValue;
+}
+
+function clearInput()
+{
+
+}
+
+function loadInputs()
+{
+    divSelect.selectedIndex = 0;
+}
+
+function removeClassList(elem,removeClass)
+{
+    elem.classList.remove(removeClass);
+}
+
+function addClassList(elem,newClass)
+{
+    elem.classList.add(newClass);
+}
+
+//Buttons
+upBtn.onclick = function(){
+
+    if(getSelectedDiv() == 'div1')
+    {
+        obj1.moveUp();
+    }
+    else if(getSelectedDiv() == 'div2')
+    {
+        obj2.moveUp();
+    }
+};
+
+downBtn.onclick = function(){
+
+    if(getSelectedDiv() == 'div1')
+    {
+        obj1.moveDown();
+    }
+    else if(getSelectedDiv() == 'div2')
+    {
+        obj2.moveDown();
+    }
+};
+
+leftBtn.onclick = function(){
+
+    if(getSelectedDiv() == 'div1')
+    {
+        obj1.moveLeft();
+    }
+    else if(getSelectedDiv() == 'div2')
+    {
+        obj2.moveLeft();
+    }
+};
+
+rightBtn.onclick = function(){
+
+    if(getSelectedDiv() == 'div1')
+    {
+        obj1.moveRight();
+    }
+    else if(getSelectedDiv() == 'div2')
+    {
+        obj2.moveRight();
+    }
+};
+
+
+
+window.onload = function(){
+    clearInput();
+    loadInputs();
+
+    obj1 = new moveDivObj(div1,1);
+    obj2 = new moveDivObj(div2,5);
+
+};
+
---
+++ lesson6/scripts/variables.js
@@ -0,0 +1,28 @@
+//variables
+
+var obj1 = null;
+var obj2 = null;
+
+//bounderies
+var bounderiesObj = {
+    top:0,
+    left:0,
+    bottom:548,
+    right:998
+};
+
+//changing DIV
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+
+//select
+var divSelect = document.getElementById('select-div');
+
+//buttons
+var downBtn = document.getElementById('downBtn');
+var upBtn = document.getElementById('upBtn');
+var leftBtn = document.getElementById('leftBtn');
+var rightBtn = document.getElementById('rightBtn');
+
+
+
