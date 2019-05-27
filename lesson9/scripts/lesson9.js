84f9af8c5fc98ff3475baaa85133cbb8fe7415d1
Matthew Martin <mar17132@byui.edu>
2019-05-26 20:08:50 -0600

    kk

---
+++ lesson9/scripts/lesson9.js
@@ -0,0 +1,120 @@
+function getSelectedDiv()
+{
+    if(divSelect.value != 'null')
+    {
+        return divSelect.value;
+    }
+
+    return null;
+}
+
+function getCSSPropVal(cssSelector,cssProp)
+{
+    var cssArrayObj = document.styleSheets;
+    var returnValue = false;
+
+    for(i = 0; i < cssArrayObj.length; i++)
+    {
+        var rulesArray = cssArrayObj[i].cssRules;
+
+        for(j = 0; j < rulesArray.length; j++)
+        {
+            var noSpaceSelector = rulesArray[j].selectorText.replace(' ','');
+
+            if(noSpaceSelector == cssSelector)
+            {
+                returnValue = rulesArray[j].style[cssProp];
+            }
+        }
+    }
+
+    return returnValue;
+}
+
+function setCSSPropVal(cssSelector,cssProp,cssVal)
+{
+    var cssArrayObj = document.styleSheets;
+    var returnValue = false;
+
+    for(i = 0; i < cssArrayObj.length; i++)
+    {
+        var rulesArray = cssArrayObj[i].cssRules;
+
+        for(j = 0; j < rulesArray.length; j++)
+        {
+            var noSpaceSelector = rulesArray[j].selectorText.replace(' ','');
+
+            if(noSpaceSelector == cssSelector)
+            {
+                rulesArray[j].style.setProperty(cssProp,cssVal);
+            }
+        }
+    }
+
+    return returnValue;
+}
+
+function clearInput()
+{
+
+}
+
+function loadInputs()
+{
+    divSelect.selectedIndex = 0;
+}
+
+function removeClassList(elem,removeClass)
+{
+    elem.classList.remove(removeClass);
+}
+
+function addClassList(elem,newClass)
+{
+    elem.classList.add(newClass);
+}
+
+//Buttons
+
+
+animeBtn.onclick = function(){
+    if(this.value == "Start Animation")
+    {
+        this.value = "Stop Animation";
+        removeClassList(div1,"stop-Animation");
+        removeClassList(div2,"stop-Animation");
+    }
+    else
+    {
+        this.value = "Start Animation";
+       // div1.style.top = div1.offsetTop + "px";
+        addClassList(div1,"stop-Animation");
+       // div2.style.left = div2.offsetLeft + "px";
+        addClassList(div2,"stop-Animation");
+    }
+};
+
+transBtn.onclick = function(){
+    if(this.value == "Start Transitions")
+    {
+        this.value = "Reset Transitions";
+        addClassList(div1,"growW");
+        addClassList(div2,"growH");
+    }
+    else
+    {
+        this.value = "Start Transitions";
+        removeClassList(div1,"growW");
+        removeClassList(div2,"growH");
+    }
+};
+
+window.onload = function(){
+    clearInput();
+    loadInputs();
+
+    obj1 = new moveDivObj(div1,1);
+    obj2 = new moveDivObj(div2,5);
+
+};
+
---
+++ lesson9/scripts/variables.js
@@ -0,0 +1,25 @@
+//variables
+
+var obj1 = null;
+var obj2 = null;
+
+//bounderies
+var bounderiesObj = {
+    top:0,
+    left:0,
+    bottom:548,
+    right:998
+};
+
+//changing DIV
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+
+//select
+
+//buttons
+var animeBtn = document.getElementById('animatBtn');
+var transBtn = document.getElementById('transBtn');
+
+
+
