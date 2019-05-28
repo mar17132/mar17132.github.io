0893a1f4fd1bdfaef10a666e0e2100278ce3f93b
Matthew Martin <mar17132@byui.edu>
2019-05-27 19:51:12 -0600

    ff

---
+++ lesson5/scripts/moveDivObj.class.js
@@ -0,0 +1,167 @@
+function moveDivObj(elemObj,moveAmmount = 1)
+{
+    myDivObj.call(this,elemObj);
+
+    this.amountMove = Number.isInteger(moveAmmount) ? moveAmmount : null;
+
+    if(this.amountMove == null)
+    {
+        console.log("Error: Bad input for move amount.");
+    }
+
+}
+
+moveDivObj.prototype = Object.create(myDivObj.prototype);
+
+moveDivObj.prototype.getAmountMove = function(){
+    return this.amountMove;
+};
+
+moveDivObj.prototype.setAmountMove = function(newMove){
+    this.amountMove = newMove;
+};
+
+moveDivObj.prototype.moveUp = function(moveAmmount = this.amountMove){
+
+    if(this.amountMove != null)
+    {
+        if(this.elem != null)
+        {
+            var thisTopOff = 0;
+
+            thisTopOff = this.elem.offsetTop;
+
+            this.elem.style.top = (thisTopOff - moveAmmount) + "px";
+
+            if(!this.isBound())
+            {
+                this.elem.style.top = thisTopOff + "px";
+            }
+        }
+    }
+    else
+    {
+        console.log("Error: Bad input for move amount.");
+    }
+
+};
+
+moveDivObj.prototype.moveDown = function moveDown(moveAmmount = this.amountMove){
+
+    if(this.amountMove != null)
+    {
+        if(this.elem != null)
+        {
+            var thisBottomOff = 0;
+
+            thisBottomOff = this.elem.offsetTop;
+
+            this.elem.style.top = (thisBottomOff + moveAmmount) + "px";
+
+            if(!this.isBound())
+            {
+                this.elem.style.top = (bounderiesObj.bottom - this.getHigh()) + "px";
+            }
+        }
+    }
+    else
+    {
+        console.log("Error: Bad input for move amount.");
+    }
+
+};
+
+moveDivObj.prototype.moveLeft = function(moveAmmount = this.amountMove){
+
+    if(this.amountMove != null)
+    {
+        if(this.elem != null)
+        {
+            var thisLeftOff = 0;
+
+            thisLeftOff = this.elem.offsetLeft;
+
+            this.elem.style.left = (thisLeftOff - moveAmmount) + "px";
+
+            if(!this.isBound())
+            {
+                this.elem.style.left = thisLeftOff + "px";
+            }
+        }
+    }
+    else
+    {
+        console.log("Error: Bad input for move amount.");
+    }
+
+};
+
+moveDivObj.prototype.moveRight = function(moveAmmount = this.amountMove){
+
+    if(this.amountMove != null)
+    {
+        if(this.elem != null)
+        {
+            var thisRightOff = 0;
+
+            thisRightOff = this.elem.offsetLeft;
+
+            this.elem.style.left = (thisRightOff + moveAmmount) + "px";
+
+            if(!this.isBound())
+            {
+                this.elem.style.left = (bounderiesObj.right - this.getWide()) + "px";
+            }
+        }
+    }
+    else
+    {
+        console.log("Error: Bad input for move amount.");
+    }
+
+};
+
+moveDivObj.prototype.isBound = function(){
+
+    var thisTop = 0;
+    var thisBottom = 0;
+    var thisLeft = 0;
+    var thisRight = 0;
+    var thisWidth = 0;
+    var thisHeight = 0;
+    var isTop = false;
+    var isLeft = false;
+    var isRight = false;
+    var isBottom = false;
+    var bound = false;
+
+    //width and height can be offsets need to test the
+    //bound setting as with the padding
+
+    thisTop = this.elem.offsetTop;
+    thisLeft = this.elem.offsetLeft;
+    thisWidth = this.elem.clientWidth;
+    thisHeight = this.elem.clientHeight;
+    thisRight = thisLeft + thisWidth;
+    thisBottom = thisTop + thisHeight;
+
+    //compare top
+    isTop = (bounderiesObj.top <= thisTop) ? true : false;
+
+    //compare left
+    isLeft = (bounderiesObj.left <= thisLeft) ? true : false;
+
+    //compare right
+    isRight = (bounderiesObj.right >= thisRight) ? true : false;
+
+    //compare bottom
+    isBottom = (bounderiesObj.bottom >= thisBottom) ? true : false;
+
+    if(isTop && isLeft && isRight && isBottom)
+    {
+        bound = true;
+    }
+
+    return bound;
+};
+
---
+++ lesson5/scripts/variables.js
@@ -0,0 +1,30 @@
+//variables
+var divCanvase = document.getElementById('divCanvas');
+var divArray = Array();
+
+//bounderies
+var bounderiesObj = {
+    top:0,
+    left:0,
+    bottom:548,
+    right:998
+};
+
+//select
+var divSelect = document.getElementById('select-div');
+var beforeSelect = document.getElementById('addBefore');
+
+//buttons
+var downBtn = document.getElementById('downBtn');
+var upBtn = document.getElementById('upBtn');
+var leftBtn = document.getElementById('leftBtn');
+var rightBtn = document.getElementById('rightBtn');
+var removeBtn = document.getElementById('removeBtn');
+var addBtn = document.getElementById('addBtn');
+
+//input
+var widthTxt = document.getElementById('newWidth');
+var heightTxt = document.getElementById('newHeight');
+var borderCTxt = document.getElementById('newBorderColor');
+var backgroundCTxt = document.getElementById('newBackColor');
+
