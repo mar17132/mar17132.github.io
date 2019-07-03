31718eb3c11112a284c5089361edf625fcb8290f
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-07-03 15:48:39 -0600

    fff

---
+++ objtest.js
@@ -0,0 +1,34 @@
+
+function questionObj()
+{
+    this.questionArray = [];
+
+}
+
+
+function songsObj()
+{
+    this.songArray = [];
+}
+
+
+function songObj()
+{
+    this.artist = "";
+    this.song = "";
+    this.cdArt = "";
+    this.album = "";
+    this.year = "";
+
+    this.hasBeenUse = false;
+    this.isAnwser = false;
+    this.hasBeenAnwser = false;
+}
+
+function userObj()
+{
+    this.numCorrect = 0;
+    this.numIncorrect = 0;
+    this.fastestTime = 0;
+}
+
