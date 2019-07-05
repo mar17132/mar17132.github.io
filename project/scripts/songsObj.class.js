7799042df41770a6fcbc6e7c8bc043d45556b029
Matthew Martin <mar17132@byui.edu>
2019-07-04 20:06:46 -0600

    ff

--- project/scripts/songsObj.class.js
+++ project/scripts/songsObj.class.js
@@ -3,7 +3,7 @@ function songsObj()
     this.songArray = [];
     this.totalNumSongs = 200;
     this.totalPageNum = 2;
-    this.responsObj;
+    this.responsObj = null;
     this.apikey = "c51c885aa28518b28abb7fd7b889fd13";
     this.apiUrl = "https://api.musixmatch.com/ws/1.1/";
     this.trackEndpoint = "chart.tracks.get";
@@ -14,33 +14,48 @@ function songsObj()
 
 songsObj.prototype.getSongsLoop = function(){
     //https://api.musixmatch.com/ws/1.1/chart.tracks.get?
-    //apikey=c51c885aa28518b28abb7fd7b889fd13&country=usa&
+    //apikey=c51c885aa28518b28abb7fd7b889fd13&country=us&
     //page=1&page_size=100&f_has_lyrics=1
 
-    test = this;
+    currentObj = this;
     xhttp = new XMLHttpRequest();
     jsonURL = this.apiUrl + this.trackEndpoint + "?apikey=" + this.apikey +
-              "&country=usa&page=1&page_size=100&f_has_lyrics=1";
+              "&format=jsonp&callback=callback&country=us&page=1&page_size=100&f_has_lyrics=1";
     xhttp.onreadystatechange = function(){
 
         console.log(this.readyState);
         console.log(this.status);
         if(this.readyState == 4 && this.status == 200)
         {
-            console.log("this worked");
-            test.apiReturn(xhttp);
+            currentObj.setResponsObj(this.responseText);
+            currentObj.apiReturn();
         }
     };
+
     xhttp.open("GET",jsonURL,true);
     xhttp.send();
 
 };
 
 
-songsObj.prototype.apiReturn = function(xhttpObj){
+songsObj.prototype.setResponsObj = function(respons){
+    respons = respons.replace("callback(","");
+    respons = respons.replace(");","");
+    console.log(respons);
+    this.responsObj = JSON.parse(respons).message.body.track_list;
+};
 
-    if(xhttpObj != null)
-        console.log('the call');
+
+songsObj.prototype.setSongsArray(){
+
+};
+
+songsObj.prototype.apiReturn = function(){
+
+    for(i = 0; i < this.responsObj; i++)
+    {
+
+    }
 };
 
 
@@ -53,5 +68,5 @@ songsObj.prototype.getRandomNum = function(limit = 1){
 
 var test = new songsObj();
 
-//test.getSongsLoop();
+test.getSongsLoop();
 
