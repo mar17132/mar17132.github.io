81bc6508cf95040bba1965680b2974acdbcbd11d
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-07-05 10:41:43 -0600

    fff

--- project/scripts/songsObj.class.js
+++ project/scripts/songsObj.class.js
@@ -41,13 +41,79 @@ songsObj.prototype.getSongsLoop = function(){
 songsObj.prototype.setResponsObj = function(respons){
     respons = respons.replace("callback(","");
     respons = respons.replace(");","");
-    console.log(respons);
     this.responsObj = JSON.parse(respons).message.body.track_list;
 };
 
+/*
+{"track_id":167685162,
+"track_name":"Old Town Road (Remix)",
+"track_name_translation_list":[],
+"track_rating":99,
+"commontrack_id":94198354,
+"instrumental":0,
+"explicit":0,
+"has_lyrics":1,
+"has_subtitles":1,
+"has_richsync":1,
+"num_favourite":1751,
+"album_id":31936342,
+"album_name":"Old Town Road (Remix)",
+"artist_id":38099281,
+"artist_name":"Lil Nas X feat. Billy Ray Cyrus",
+"restricted":0,
+"updated_time":"2019-04-05T12:54:48Z"
+*/
+songsObj.prototype.setSongsArray = function(song){
+    var newSong = new songObj();
+    newSong.setAlbum(song.album_name);
+    newSong.setAlbumId(song.album_id);
+    newSong.setSong(song.track_name);
+    newSong.setSongId(song.track_id);
+    newSong.setArtist(song.artist_name);
+    newSong.setArtistId(song.artist_id);
+    this.songArray.push(newSong);
+    this.setAlbYearArt();
+    this.setSongLyrics();
+};
 
-songsObj.prototype.setSongsArray(){
+songsObj.prototype.setAlbYearArt = function(){
+    currentObj = this;
+    xhttp = new XMLHttpRequest();
+    jsonURL = this.apiUrl + this.albumEndpoint + "?apikey=" + this.apikey +
+              "&format=jsonp&callback=callback&";
+    xhttp.onreadystatechange = function(){
 
+        console.log(this.readyState);
+        console.log(this.status);
+        if(this.readyState == 4 && this.status == 200)
+        {
+            var albumRespons = JSON.parse(this.responseText);
+            currentObj.apiReturn();
+        }
+    };
+
+    xhttp.open("GET",jsonURL,true);
+    xhttp.send();
+};
+
+songsObj.prototype.setSongLyrics = function(){
+    currentObj = this;
+    xhttp = new XMLHttpRequest();
+    jsonURL = this.apiUrl + this.trackEndpoint + "?apikey=" + this.apikey +
+              "&format=jsonp&callback=callback&";
+    xhttp.onreadystatechange = function(){
+
+        console.log(this.readyState);
+        console.log(this.status);
+        if(this.readyState == 4 && this.status == 200)
+        {
+            var lyricRespons = JSON.parse(this.responseText);
+            currentObj.apiReturn();
+        }
+    };
+
+    xhttp.open("GET",jsonURL,true);
+    xhttp.send();
 };
 
 songsObj.prototype.apiReturn = function(){
@@ -57,7 +123,6 @@ songsObj.prototype.apiReturn = function(){
 
     }
 };
-
 
 songsObj.prototype.getRandomNum = function(limit = 1){
 
@@ -69,4 +134,5 @@ songsObj.prototype.getRandomNum = function(limit = 1){
 var test = new songsObj();
 
 test.getSongsLoop();
+
 
