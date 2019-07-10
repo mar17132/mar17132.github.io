a8f18a4b16446690d932c0d16ee87471affb4a2c
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-07-10 09:28:58 -0600

    createsong

--- project/scripts/songsObj.class.js
+++ project/scripts/songsObj.class.js
@@ -25,4 +25,27 @@ songsObj.prototype.getSongRandom = function(){
 };
 
 
+songsObj.prototype.createSongsArray = function(objArray){
+    for(i = 0; i < objArray.length; i++)
+    {
+        objSong = objArray[i];
+
+        //create new song 
+        song = new songObj();
+        song.setArtist(objSong.artist);        
+        song.setArtistId(objSong.artistId);        
+        song.setSong(objSong.song);        
+        song.setSongId(objSong.songId);        
+        song.setCdArt(objSong.cdArt);        
+        song.setAlbum(objSong.album);        
+        song.setAlbumId(objSong.albumId);        
+        song.setYear(objSong.year);        
+        song.setLyrics(objSong.lyrics);
+
+        //add song to array
+        this.songsArray[i] = song;
+        
+    }
+};
+
 
