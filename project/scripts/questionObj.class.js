d971df98862cc1df13be1d065f7896116914e65f
Matthew Martin <43966328+mar17132@users.noreply.github.com>
2019-07-10 12:27:05 -0600

    filtering update

--- project/scripts/questionObj.class.js
+++ project/scripts/questionObj.class.js
@@ -9,7 +9,7 @@ function questionObj()
 questionObj.prototype.getSongAnwsers = function(){
     for(i = 0; i < this.numberAnwsers; i++)
     {
-        song = this.songsObj.getSongRandom(this.songsObj.getArraySize());
+        song = this.songsObj.getSongRandom(this.songsObj.getArraySize());                 
         song.setBeenUse(true);
         this.questionArray.push(song);
     }
@@ -20,6 +20,15 @@ questionObj.prototype.setSongsObj = function(songs){
 };
 
 questionObj.prototype.setQuestionAnwser = function(){
+           // if(song.hasBeenAnwser() == false)
     this.songsObj.getSong(this.numberAnwsers).setAnwser(true);
+};
+
+questionObj.prototype.clear = function(){
+    for(i = 0; i < this.numberAnwsers; i++)
+    {
+        song = this.questionArray.pop();  
+        song = null;
+    }
 };
 
